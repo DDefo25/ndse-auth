6902a60a8fd144cf7924aabe4ba2a790430626db
Maksim Pumé <94569453+DDefo25@users.noreply.github.com>
2023-09-13 15:05:39 +0000

    intitial

---
+++ db/users.js
@@ -0,0 +1,70 @@
+const { v4: uuid } = require('uuid')
+
+const records = [
+    {
+      id: 1,
+      username: 'user',
+      password: '123456',
+      displayName: 'demo user',
+      email: 'user@mail.ru',
+    },
+    {
+      id: 2,
+      username: 'jill',
+      password: 'birthday',
+      displayName: 'Jill',
+      email: 'jill@example.com',
+    },
+  ]
+  
+const findById = function (id, cb) {
+    process.nextTick(function () {
+        const record = records.find(user => user.id === id);
+        if (record) {
+            cb(null, record)
+        } else {
+            cb(new Error('User ' + id + ' does not exist'))
+        }
+    })
+  }
+  
+  const findByUsername = function (username, cb) {
+    process.nextTick(function () {
+        const record = records.find(user => user.username === username);
+        if (record) {
+          return cb(null, record)
+        }
+      return cb(null, null)
+    })
+  }
+  
+const verifyPassword = (user, password) => {
+    return user.password === password
+  }
+
+const addUser = function (user, cb) {
+    const {username, password, displayName, email} = user;
+    process.nextTick(function () {
+        if (records.some(user => user.username === username)) {
+            cb(new Error('User ' + username + ' has already exist'))
+        } else {
+            const record = {
+                id: uuid(),
+                username,
+                password,
+                displayName,
+                email,
+            }
+            records.push(record)
+            return cb(null, record)
+        }
+      return cb(null, null);
+    })
+  }
+
+  module.exports = {
+    findById,
+    findByUsername,
+    verifyPassword,
+    addUser
+  }
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const indexRoute = require('./routes/api/index');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+const port = process.env.PORT || 8989;
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(indexRoute)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
